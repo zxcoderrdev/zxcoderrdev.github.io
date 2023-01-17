35983b3a45a778890770e83b6f541c2c1854351d
zxcoderrdev <122905755+zxcoderrdev@users.noreply.github.com>
2023-01-17 21:53:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -156,6 +156,36 @@ function fn2() {
   alert('Coming soon...')
   return false;
 }
+
+document.onmousemove = animateCircles; // circle follow mouse
+
+var colors = ['#1abc9c', '#3498db', '#f1c40f']
+
+function animateCircles(event) {
+  var circle = document.createElement("div");
+  circle.setAttribute("class", "circle");
+  document.body.appendChild(circle); // adds function to body
+
+  // adds motion
+  circle.style.left = event.clientX + 'px';
+  circle.style.top = event.clientY + 'px';
+
+  // randomize color
+  var color = colors[Math.floor(Math.random() * colors.length)];
+  circle.style.borderColor = color;
+
+  // adds animation
+  circle.style.transition = "all 0.5s linear 0s";
+
+  circle.style.left = circle.offsetLeft - 20 + 'px';
+  circle.style.top = circle.offsetTop - 20 + 'px';
+
+  circle.style.width = "50px";
+  circle.style.height = "50px";
+  circle.style.borderWidth = "5px";
+  circle.style.opacity = 0;
+}
+
 // FULL OR PARTIAL COPYING OF FILES IS PROHIBITED OR CONTACT THE OWNER in Telegram @zxcoderr
 // FULL OR PARTIAL COPYING OF FILES IS PROHIBITED OR CONTACT THE OWNER in Telegram @zxcoderr
 // FULL OR PARTIAL COPYING OF FILES IS PROHIBITED OR CONTACT THE OWNER in Telegram @zxcoderr
